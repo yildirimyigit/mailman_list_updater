7cf7708e0354c286e6c67fa50f940f2feec8abb7
yigit <yigyil@gmail.com>
2019-09-03 01:48:58 +0300

    Done

--- upload.php
+++ upload.php
@@ -9,7 +9,7 @@ function findCol($sheet){
     $col = -1;
     $mailRowStart = -1;
     $lastRow = $sheet->getHighestRow();
-    $lastCol = min(PHPExcel_Cell::columnIndexFromString($sheet->getHighestColumn()), 100); //highestColumn() returns interesting numbers. 100 is just a fine limit.
+    $lastCol = min(PHPExcel_Cell::columnIndexFromString($sheet->getHighestColumn()), 100); //highestColumn() returns interesting numbers. 100 is$
     for($i = 0; $i < $lastCol; $i++) {  //col starts from 0
         for ($j = 1; $j <= $lastRow; $j++) {    //row starts from 1 and first 2 rows are for the headers
             $data = $sheet->getCellByColumnAndRow($i, $j)->getValue();
@@ -36,7 +36,7 @@ function extractMailAddresses($sheet, $col, $rowStart, $rowEnd){
 }
 
 
-$targetDir = "uploads/";
+$targetDir = "data/";
 $targetFile = $targetDir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 
@@ -47,12 +47,13 @@ if ($fileType != "xlsx") {
     echo "Only .xlsx files are allowed.";
     $uploadOk = 0;
 }
-
+else die("NO");
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
-} else {
+}
+else {
     if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
         $xlReader = PHPExcel_IOFactory::createReaderForFile($targetFile);
         $xlObj = $xlReader->load($targetFile);
@@ -68,18 +69,37 @@ if ($uploadOk == 0) {
             die("Email column not found!");
         else{
             $listName = $_POST['list'];
-            echo $listName."<br>---<br><br>";
+
+            // archive the xlsx
+            $newFolder = 'data/archive/'.$listName."-".date('Ymd-His');
+            mkdir($newFolder);
+            rename($targetFile, $newFolder.'/uploaded.xlsx');
+
+            $command = escapeshellcmd('/var/www/html/panel/list_members '.$listName);
+            $oldMembersFilePath = $newFolder. "/old_members.txt";
+            $output = shell_exec("python " .$command. " > ".$oldMembersFilePath);
+
+            $newMembersFilePath = $newFolder . "/new_members.txt";
+            $newMembersFile = fopen($newMembersFilePath, "w");
+
             foreach ($mails as $mail)
-                echo $mail."<br>";
-            // TODO: write to file and call ./add_members -r list file
+                fwrite($newMembersFile, $mail."\n");
+            fclose($newMembersFile);
+
+            $removeCommand = escapeshellcmd('/var/www/html/panel/remove_members --nouserack --noadminack -f '. $oldMembersFilePath ." ".$listName);
+            $removeCmdOutput = shell_exec("python ".$removeCommand);
+
+            $addCommand = escapeshellcmd('/var/www/html/panel/add_members -r '. $newMembersFilePath ." ".$listName);
+            $addCmdOutput = shell_exec("python ".$addCommand);
+
         }
-        // archive the xlsx
-        rename($targetFile, 'uploads/archive/'.$listName."-".date('Ymd-His'));
 
     }
     else{
         echo "Sorry, there was an error uploading your file.";
     }
 }
+
+?>
 
 ?>
